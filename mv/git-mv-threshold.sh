73d6ea4b27d92c74dc6dc1cba2f6e5eb3c7fdad0
Kevin Welton <kevinwe@unity3d.com>
2022-12-13 08:59:46 -0800

    Fix git-mv-threshold.sh script. It was just a copy of git-mv-simple.sh.

--- mv/git-mv-threshold.sh
+++ mv/git-mv-threshold.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-folder="git-mv"
+folder="git-mv-threshold"
 
 if [ -d "./$folder" ]; then
-    rm -rf git-mv
+    rm -rf $folder
 fi
 
 mkdir $folder && cd $_
@@ -20,21 +20,21 @@ cp file1 file2
 git add file1 file2
 git commit -m "Add a couple of small files"
 
+printf "\n>>> Append files: file1 within rename threshold, file2 outside threshold."
+printf "\n"
+echo defghij >> file1
+echo defghijk >> file2
+
 printf "\n>>> Rename via git mv takes one line to rename and stage:"
-printf "\n>>> git mv file1 rename-via-git-mv"
+printf "\n>>> git mv file1 rename-file-1"
+printf "\n>>> git mv file2 rename-file-2"
+printf "\n>>> git add *"
 printf "\n"
-git mv file1 rename-via-git-mv
+git mv file1 rename-file1
+git mv file2 rename-file2
+git add *
 
-printf "\n>>> Rename via simple mv takes three lines to rename and stage:"
-printf "\n>>> mv file2 rename-via-bash-mv"
-printf "\n>>> git add file2              # Add deleted file"
-printf "\n>>> git add rename-via-bash-mv # Add new (moved) file"
-printf "\n"
-mv file2 rename-via-bash-mv
-git add file2
-git add rename-via-bash-mv
-
-printf "\n>>> git status shows that git detects both as renames:"
+printf "\n>>> git status shows that git detects file1 as a rename, but file2 as a new file:"
 printf "\n"
 git status
 
